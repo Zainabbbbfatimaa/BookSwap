b3bfd36f4af60115b39a7a527ca65e3d3dd49c37
Zainab Fatima <zainab.fatima8485@gmail.com>
2026-06-16 11:59:38 +0500

    fix login api url

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/users/login",
+        "https://bookswap-9ry5.onrender.com/api/users/login" ,
         formData
       );
 
